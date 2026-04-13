b1cf123c77488dc1bdc69f98729aa65d39bba23e
Egor <egor@mail.ru>
2026-04-13 14:17:18 +0700

    разобрался с depends

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from typing import Annotated
 
 import uvicorn
 from fastapi import FastAPI, Depends
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
@@ -19,6 +18,17 @@ async def get_session():
     async with new_session() as session:
         yield session # держит сессию открытой, пока не вернется ответ пользователю
 
+
+#test.py типа прописываем в файле с тестами
+
+async def test_get_session():
+    async with new_session() as session:
+        yield session # держит сессию открытой, пока не вернется ответ пользователю
+
+
+app.dependency_overrides[get_session] = test_get_session # теперь вместо зависимости get_session,
+                                                         # будет использоваться test_get_session
+# ========
 
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
@@ -58,10 +68,22 @@ async def add_book(data: BookAddSchema, session: SessionDep):
     await session.commit()
     return {'ok': True}
 
+class PaginationParams(BaseModel):
+    limit: int = Field(5, ge=0, le=100, description='Кол-во элементов на странице')
+    offset: int = Field(0, ge=0, description='Смещение для пагинации')
+
+
+PaginationDep = Annotated[PaginationParams, Depends(PaginationParams)]
 @app.get('/books')
-async def get_books(session: SessionDep):
-    query = select(BookModel)
-    await session.execute(query)
+async def get_books(
+        session: SessionDep,
+        pagination: PaginationDep,
+) -> list[BookSchema]:
+    query = (
+        select(BookModel)
+        .limit(pagination.limit)
+        .offset(pagination.offset)
+    )
     result = await session.execute(query)
     return result.scalars().all()
 
